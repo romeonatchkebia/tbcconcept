9bc54bcccc212061d9e1b70d0ba34714c8d3c895
romeonatchkebia <121379511+romeonatchkebia@users.noreply.github.com>
2024-07-26 22:04:48 +0400

    initial version add of offers slider

--- js/main.js
+++ js/main.js
@@ -4,3 +4,33 @@ document.addEventListener("DOMContentLoaded", () => {
     menuButton.classList.toggle("active");
   });
 });
+
+const swiper = new Swiper(".swiper", {
+  loop: false,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 5,
+    },
+    600: {
+      slidesPerView: 1.6,
+    },
+    930: {
+      slidesPerView: 2.5,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+});
