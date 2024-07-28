4264b807bcae982c876d2bf441c5e895a90be9c5
romeonatchkebia <121379511+romeonatchkebia@users.noreply.github.com>
2024-07-28 18:43:19 +0400

    links added to buttons

--- js/contactCard.js
+++ js/contactCard.js
@@ -2,25 +2,27 @@ const contactsData = [
   {
     img: "phone.svg",
     info: "+995 32 2 27 27 00",
+    href: "#",
   },
   {
     img: "envelope.svg",
     info: "info@tbcconcept.ge",
+    href: "mailto:info@tbcconcept.ge",
   },
   {
     img: "location.svg",
     info: "Branches",
+    href: "https://tbcconcept.ge/en/concept-space/concept-branches",
   },
 ];
 
 const contactsHTML = contactsData
   .map(
-    (item) => `
-          <div class="contactsWrapper">
-            <img src="assets/icons/${item.img}" />
-            <div class="contactInfo">${item.info}</div>
-          </div>
-          `
+    (item) =>
+      `<a class="contactsWrapper" href=${item.href} style="text-decoration: none;">
+          <img src="assets/icons/${item.img}"/>
+          <div class="contactInfo">${item.info}</div>
+        </a>`
   )
   .join("");
 
